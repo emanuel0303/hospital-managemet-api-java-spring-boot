93ec7a2f6e472a1ba6987e399342c41b890e7b1b
Emanuel Soloman <165172845+emanuel0303@users.noreply.github.com>
2024-05-10 21:07:44 +0530

    Update AddDoctorApi.java

--- Src/src/main/java/com/hospital/hospitalmanagementsystem/apis/AddDoctorApi.java
+++ Src/src/main/java/com/hospital/hospitalmanagementsystem/apis/AddDoctorApi.java
@@ -1,7 +1,9 @@
 package com.hospital.hospitalmanagementsystem.apis;
 
+import com.hospital.hospitalmanagementsystem.dto.DoctorDTO;
 import com.hospital.hospitalmanagementsystem.entity.Doctor;
 import com.hospital.hospitalmanagementsystem.service.DoctorService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,14 +11,21 @@ import org.springframework.stereotype.Component;
 public class AddDoctorApi {
 
     private final DoctorService doctorService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public AddDoctorApi(DoctorService doctorService) {
+    public AddDoctorApi(DoctorService doctorService, ModelMapper modelMapper) {
         this.doctorService = doctorService;
+        this.modelMapper = modelMapper;
     }
 
-    public String addDoctor(Doctor doctor) {
-        doctorService.addDoctor(doctor);
+    public String addDoctor(DoctorDTO doctorDTO) {
+        // Map DTO to entity
+        Doctor doctor = modelMapper.map(doctorDTO, Doctor.class);
+
+        // Add doctor using the service
+        doctorService.addDoctor(doctorDTO);
+
         return "Doctor Added successfully";
     }
 }
