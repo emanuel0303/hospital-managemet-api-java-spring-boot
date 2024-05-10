cb553d8b4dcf09e3cb03350771d96ec4c921c809
Emanuel Soloman <emanuelsoloman36@gmail.com>
2024-05-10 20:36:57 +0530

    src

--- Src/src/main/java/com/hospital/hospitalmanagementsystem/service/AppointmentService.java
+++
@@ -1,7 +0,0 @@
-package com.hospital.hospitalmanagementsystem.service;
-
-public interface AppointmentService {
-    void deleteAppointment(Long appointmentId);
-
-    void deleteAppointmentsByDoctorId(Integer drid);
-}
