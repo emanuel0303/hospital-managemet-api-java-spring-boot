5e9cdc2eb865c717a2b76c8a05ca48e730d0d1bf
Emanuel Soloman <165172845+emanuel0303@users.noreply.github.com>
2024-05-11 10:16:12 +0530

    Update UpdateDoctorApi.java

--- Src/src/main/java/com/hospital/hospitalmanagementsystem/apis/UpdateDoctorApi.java
+++ Src/src/main/java/com/hospital/hospitalmanagementsystem/apis/UpdateDoctorApi.java
@@ -3,12 +3,12 @@ package com.hospital.hospitalmanagementsystem.apis;
 import com.hospital.hospitalmanagementsystem.entity.Doctor;
 import com.hospital.hospitalmanagementsystem.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
-@Service
 public class UpdateDoctorApi {
+
     private final DoctorService doctorService;
 
     @Autowired
@@ -16,8 +16,9 @@ public class UpdateDoctorApi {
         this.doctorService = doctorService;
     }
 
-    public void updateDoctor(Integer drid, Doctor doctor) {
-        // Call the appropriate method in the PatientService to update the patient
+    public ResponseEntity<String> updateDoctor(Integer drid, Doctor doctor) {
         doctorService.updateDoctorById(drid, doctor);
+        String message = "Doctor with ID " + drid + " updated successfully.";
+        return ResponseEntity.ok().body(message);
     }
 }
