1bd1727aaeebf45f5e897716cf28013efea8caf2
Emanuel Soloman <165172845+emanuel0303@users.noreply.github.com>
2024-05-11 10:27:04 +0530

    Update BookAppointmentApi.java

--- Src/src/main/java/com/hospital/hospitalmanagementsystem/apis/BookAppointmentApi.java
+++ Src/src/main/java/com/hospital/hospitalmanagementsystem/apis/BookAppointmentApi.java
@@ -1,6 +1,8 @@
 package com.hospital.hospitalmanagementsystem.apis;
 
 import com.hospital.hospitalmanagementsystem.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,13 +11,16 @@ public class BookAppointmentApi {
     private final PatientService patientService;
 
     // Constructor for BookAppointmentApi class to inject PatientService
+    @Autowired
     public BookAppointmentApi(PatientService patientService) {
         this.patientService = patientService;
     }
 
     // Method to book appointment
-    public void bookAppointment(Integer id, Integer drid, String date, String time) {
+    public ResponseEntity<String> bookAppointment(Integer id, Integer drid, String date, String time) {
         // Delegate the appointment booking logic to the PatientService
         patientService.bookAppointment(id, drid, date, time);
+        String message = "Appointment booked successfully.";
+        return ResponseEntity.ok().body(message);
     }
 }
