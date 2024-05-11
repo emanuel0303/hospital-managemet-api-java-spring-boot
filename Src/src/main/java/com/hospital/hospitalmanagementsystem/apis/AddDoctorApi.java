c90caaa0d90213c61a26033e3e2312c3685ec727
Emanuel Soloman <165172845+emanuel0303@users.noreply.github.com>
2024-05-11 11:25:58 +0530

    Update AddDoctorApi.java

--- Src/src/main/java/com/hospital/hospitalmanagementsystem/apis/AddDoctorApi.java
+++ Src/src/main/java/com/hospital/hospitalmanagementsystem/apis/AddDoctorApi.java
@@ -5,6 +5,8 @@ import com.hospital.hospitalmanagementsystem.entity.Doctor;
 import com.hospital.hospitalmanagementsystem.service.DoctorService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,18 +16,18 @@ public class AddDoctorApi {
     private final ModelMapper modelMapper;
 
     @Autowired
-    public AddDoctorApi(DoctorService doctorService, ModelMapper modelMapper) {
+    public AddDoctorApi(DoctorService doctorService,
+                        ModelMapper modelMapper) {
         this.doctorService = doctorService;
         this.modelMapper = modelMapper;
     }
 
-    public String addDoctor(DoctorDTO doctorDTO) {
-        // Map DTO to entity
-        Doctor doctor = modelMapper.map(doctorDTO, Doctor.class);
-
-        // Add doctor using the service
-        doctorService.addDoctor(doctorDTO);
-
-        return "Doctor Added successfully";
+    public ResponseEntity<String> addDoctor(DoctorDTO doctorDTO) {
+        Doctor doctor = doctorService.addDoctor(doctorDTO);
+        String successMessage = "Doctor added successfully.\n" +
+                "Doctor Name: " + doctor.getDoctorname() + "\n" +
+                "Doctor Age: " + doctor.getDrage() + "\n" +
+                "Doctor Email: " + doctor.getDremail();
+        return ResponseEntity.status(HttpStatus.CREATED).body(successMessage);
     }
 }
