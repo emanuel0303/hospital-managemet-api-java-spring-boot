4f7656c1f90ac191f9cd7849e37d292d2fb50c5a
Emanuel Soloman <165172845+emanuel0303@users.noreply.github.com>
2024-05-11 11:57:37 +0530

    Update PatientRepository.java

--- Src/src/main/java/com/hospital/hospitalmanagementsystem/repository/PatientRepository.java
+++ Src/src/main/java/com/hospital/hospitalmanagementsystem/repository/PatientRepository.java
@@ -3,7 +3,9 @@ package com.hospital.hospitalmanagementsystem.repository;
 import com.hospital.hospitalmanagementsystem.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
 
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
+    List<Patient> findByDoctorDrid(Integer doctorId);
 }
